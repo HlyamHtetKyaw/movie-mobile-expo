fc532ea7d47cce6b6c080d5ecc91623d57c70e30
hlyamhtetkyaw <hlan1559@gmail.com>
2025-11-21 20:56:49 +0630

    [ADD] skeleton movies content

---
+++ components/MovieCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from "react-native";
+
+const MovieCard = () => {
+    return (
+        <View>
+            <Text>Movie Card</Text>
+        </View>
+    );
+};
+
+export default MovieCard;
--- services/api.ts
+++ services/api.ts
@@ -1,3 +1,5 @@
+
+import Constants from "expo-constants";
 // const url = "";
 // const options ={
 //     method: 'GET',
@@ -10,21 +12,23 @@
 //     .then(res=>res.json())
 //     .then(json=>console.log(json))
 //     .catch(err=>console.log)
-
-export const MOVIE_API_CONFIG={
+const MOVIE_API_CONFIG={
     BASE_URL: 'https://www.omdbapi.com/',
-    API_KEY: process.env.MOVIE_API_KEY as string,
+    API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_KEY as string,
     headers: {
         accept: 'application/json'
     }
 }
+export default MOVIE_API_CONFIG
 
 const buildUrl = (params: Record<string, string | number>) => {
     const query = new URLSearchParams({
         apikey: MOVIE_API_CONFIG.API_KEY,
         ...params,
     });
-    return `${MOVIE_API_CONFIG.BASE_URL}?${query.toString()}`;
+    let url = `${MOVIE_API_CONFIG.BASE_URL}?${query.toString()}`;
+    console.log("calling url "+url)
+    return url;
 };
 
 export const fetchMovies = async ({ query,page }: { query: string,page:number }) => {
@@ -35,14 +39,11 @@ export const fetchMovies = async ({ query,page }: { query: string,page:number })
         });
 
         const response = await fetch(endpoint);
-        if(!response.ok){
-            throw new Error('Failed to fetch movies')
-        }
         const data = await response.json();
 
         return data;
     } catch (error) {
-        console.error("OMDb fetch error:", error);
+        console.error("OMDb fetch error ", error);
         return null;
     }
 };
